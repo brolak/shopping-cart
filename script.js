e3114790420d5d6a30a0d7923da23b664198c7d6
Mati <matiborlak@gmail.com>
2017-02-22 15:42:37 +0200

    visible cart, array of items from add buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var $cartList = $(".cartList");
-
+var $shoppingCart = $(".shopping-cart");
+var $cartList = $(".cart-list");
 // an array with all of our cart items
 var cart = [];
 
@@ -7,25 +7,38 @@ var updateCart = function () {
   // TODO: Write this function. In this function we render the page.
   // Meaning we make sure that all our cart items are displayed in the browser.
   // Remember to empty the "cart div" before you re-add all the item elements.
+  $cartList.empty();
+  var source = $('#cart-template').html();
+  var template = Handlebars.compile(source);
+  var newHTML = template({
+          cart: cart 
+        });
+  $cartList.append(newHTML);
 }
 
 
 var addItem = function (item) {
-  // TODO: Write this function. Remember this function has nothing to do with display. 
-  // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
+  //add items to cart array
+  cart.push({
+    "name": item.dataset.name,
+    "price": item.dataset.price
+  })
 }
 
 var clearCart = function () {
-  $cartList.empty();
+  //empty the shopping cart
+  $cartList.empty()
 }
 
 $('.view-cart').on('click', function () {
-  $(".shopping-cart").toggleClass(".show");
+  //display shopping cart on click
+  $shoppingCart.toggleClass('show');
 });
 
 $('.add-to-cart').on('click', function () {
-  // TODO: get the "item" object from the page
-  addItem(item);
+  //get the "item" object from the page
+  newItem = this.closest(".item")
+  addItem(newItem);
   updateCart();
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.listen(8000);
+app.listen(8080);
 
 app.use(express.static('public'));
 app.use(express.static(__dirname + '/node_modules'));
