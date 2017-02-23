ad7f830ed9ae33ed715f5794cc763972328dc04a
Yejiel Wahnich <yejielw@hotmail.com>
2017-02-23 10:14:23 +0200

    added new item html and js

---
+++ public/addItem.js
@@ -0,0 +1,22 @@
+var itemSource = $('#item-template').html();
+var itemTemplate = Handlebars.compile(itemSource);
+
+
+var addItemtoPage = function () {
+	debugger;
+	var $itemInput = $('.item-input').val();
+	var $itemPrice = $('.price-input').val();
+	var $imgUrl = $('.img-input').val()
+	 var newHTML = itemTemplate({
+	 	name: $itemInput,
+        price: $itemPrice,
+        image: $imgUrl
+        });
+	 $('.item-page').append(newHTML);
+}
+
+$('#addBtn').on('click', function () {
+	addItemtoPage();
+})
+
+
